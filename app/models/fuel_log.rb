7fe2cd4139f2286fdcff029fc589e016fe8cb272
Jan Sandbrink <nobodysnightmare@gmail.com>
2017-12-19 07:29:32 +0100

    Show more stats in log view

--- app/models/fuel_log.rb
+++ app/models/fuel_log.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 class FuelLog < ApplicationRecord
   belongs_to :car
+
+  def consumption
+    fuel_used / distance
+  end
+
+  def total_price
+    fuel_price * fuel_used
+  end
 end
