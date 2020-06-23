9b4cd6dcfd64c14f98ce969913cb4ad9e1762d80
Jan Sandbrink <nobodysnightmare@gmail.com>
2020-06-23 21:11:38 +0200

    Fix handling of unexpected values
    Sometimes errors occur and a number is NaN or
    Infinity. While this is not cool, the app shouldn't
    crash because rendering these numbers fails.

--- app/helpers/format_numbers_helper.rb
+++ app/helpers/format_numbers_helper.rb
@@ -1,5 +1,6 @@
 module FormatNumbersHelper
   def human_number(number, max_precision: 2)
+    return number if number.infinite? || (number.respond_to?(:nan?) && number.nan?)
     number = number.round(max_precision)
     number = number.to_i if number.denominator == 1 && number.is_a?(Rational)
     number_with_delimiter number
